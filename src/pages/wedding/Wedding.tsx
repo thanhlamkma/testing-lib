7bac8b3c52ee9c853637f2cbe43204a79d49ba7e
Grela <vtlam134@gmail.com>
2025-01-27 11:58:07 +0700

    feat: complete right card

--- src/pages/wedding/Wedding.tsx
+++ src/pages/wedding/Wedding.tsx
@@ -9,7 +9,14 @@ const Wedding = () => {
           <div className='lc__save-date'>Save The Date</div>
 
           <div className='lc__img-ctn'>
-            <Carousel className='w-full h-full' infinite={true} dots={false} draggable fade>
+            <Carousel
+              className='w-full h-full'
+              infinite={true}
+              dots={false}
+              draggable
+              fade
+              // autoplay
+            >
               <div>
                 <img className='lc__img' src='./images/WeddingCard5.png' alt='Image 1' />
               </div>
@@ -43,8 +50,17 @@ const Wedding = () => {
 
         {/* Right card */}
         <div className='rc'>
-          <p className='font-semibold'>TRÂN TRỌNG KÍNH MỜI</p>
-          <p>...Bạn + Người thân...</p>
+          <p className='text-xl font-bold md:text-3xl' style={{ fontFamily: 'Times New Roman' }}>
+            TRÂN TRỌNG KÍNH MỜI
+          </p>
+
+          <div className='relative w-full'>
+            <p className='absolute left-0 flex justify-center w-full italic bottom-[2px]'>
+              Bạn và người thương
+            </p>
+            <p>............................................................</p>
+          </div>
+
           <p>Đến dự lễ thành hôn của chúng tôi</p>
 
           <div className='rc__couple'>
@@ -55,23 +71,30 @@ const Wedding = () => {
 
           <p>Được tổ chức vào hồi</p>
 
-          <div className='rc__date'>
-            <p>10 giờ 30 phút</p>
-            <div>
-              <span>THỨ BẢY</span>
-              <span>15</span>
-              <span>THÁNG 3</span>
+          <div className='flex flex-col items-center justify-center gap-1 text-base font-semibold'>
+            <p className='text-base'>10 giờ 30</p>
+
+            <div className='flex items-center justify-center gap-4'>
+              <span className='py-1 border-t border-b border-solid'>THỨ BẢY</span>
+              <span className='flex items-center justify-center w-12 h-12 text-3xl font-semibold border border-solid rounded-full text-[#da312e]'>
+                15
+              </span>
+              <span className='py-1 border-t border-b border-solid'>03 - 2025</span>
             </div>
-            <p>2025</p>
-            <p>(Tức ngày 16 tháng 02 năm Tân Tỵ)</p>
+
+            {/* <p className='text-base'>2025</p> */}
+
+            <p className='text-sm'>(Tức ngày 16 tháng 02 năm Tân Tỵ)</p>
           </div>
 
-          <div>
-            <p>Tại tư gia nhà trai</p>
-            <p>Xóm 2 - Xuân Châu - Xuân Trường - Nam Định</p>
+          <div className='my-1 text-base font-medium font-[Mulish]'>
+            <p>Tại: Tư gia nhà trai</p>
+            <p>Xóm 2, Xuân Châu, Xuân Trường, Nam Định</p>
           </div>
 
-          <div>Sự hiện diện của quý khách là niềm vinh dự cho gia đình chúng tôi</div>
+          <div className='text-2xl font-[Santillana-Daughter] leading-6'>
+            Sự hiện diện của quý khách là niềm vinh dự cho gia đình chúng tôi!
+          </div>
         </div>
       </div>
     </div>
